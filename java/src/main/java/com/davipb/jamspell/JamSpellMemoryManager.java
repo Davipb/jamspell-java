ec07799d5ce1ad1264b8dd230f63fb096b9fe8be
Davipb <daviparca@gmail.com>
2018-07-16 20:20:15 -0300

    Abstract platform detection into its own class

--- java/src/main/java/com/davipb/jamspell/JamSpellMemoryManager.java
+++ java/src/main/java/com/davipb/jamspell/JamSpellMemoryManager.java
@@ -2,9 +2,6 @@ package com.davipb.jamspell;
 
 import lombok.NonNull;
 import lombok.val;
-import org.apache.commons.lang3.ArchUtils;
-import org.apache.commons.lang3.SystemUtils;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -93,11 +90,8 @@ final class JamSpellMemoryManager {
      * If a resource is found at the path {@code /com/davipb/jamspell/override}, regardless of its contents,
      * the loading of libraries is aborted immediately.
      * <p>
-     * Otherwise, a resource is looked up at {@code /com/davipb/jamspell/<os>/<arch>/<bit>/jamspell-jni-name}, where
-     * {@code <os>} is an identifier for the current Operating System (Windows, Linux, etc), {@code <arch>} is an
-     * identifier for the current processor architecture (x86, Itanium, PowerPC, etc), and {@code <bit>} is an
-     * identifier for the current processor bitness (32-bit, 64-bit, etc). For the exact identifiers used, see
-     * {@link #getOsIdentifier()}, {@link #getArchIdentifier()}, and {@link #getBitnessIdentifier()}.
+     * Otherwise, a resource is looked up at {@code /com/davipb/jamspell/<platform>/jamspell-jni-name}, where
+     * {@code <platform>} is a platform-specific path specified by {@link PlatformUtils#PLATFORM_PATH}
      * <p>
      * If that resource is found, its contents are read and interpreted as the resource path to the native library
      * that will be loaded. If the path starts with {@code /}, it is interpreted as an absolute path, starting at the
@@ -108,9 +102,10 @@ final class JamSpellMemoryManager {
      *
      * @throws JamSpellException If a {@code jamspell-jni-name} resource contains an invalid path or if the system is
      *                           unable to copy the native library to disk.
-     * @implNote If the current OS/Architecture/Bitness doesn't have a {@code jamspell-jni-name}, the library loading is
+     * @implNote If the current platform doesn't have a {@code jamspell-jni-name}, the library loading is
      * aborted without an error. This is to allow for external loading of native libraries by users of this class without
      * the need to override existing native libraries with an {@code override} file.
+     * @see PlatformUtils#PLATFORM_PATH
      */
     static synchronized void initialize() throws JamSpellException {
         if (initialized) return;
@@ -121,7 +116,7 @@ final class JamSpellMemoryManager {
         val override = loader.getResource(LIBRARY_BASE + "/override");
         if (override != null) return;
 
-        val pathBase = String.join("/", LIBRARY_BASE, getOsIdentifier(), getArchIdentifier(), getBitnessIdentifier());
+        val pathBase = LIBRARY_BASE + "/" + PlatformUtils.PLATFORM_PATH;
         val descriptorPath = String.join("/", pathBase, "jamspell-jni-name");
 
         val descriptor = loader.getResourceAsStream(descriptorPath);
@@ -154,70 +149,5 @@ final class JamSpellMemoryManager {
 
     }
 
-    /**
-     * Gets the identifier for the current Operational System. The exact identifiers are:
-     * <ul>
-     * <li>{@code windows} &mdash; For any Microsoft Windows version.</li>
-     * <li>{@code linux} &mdash; For any system using the Linux kernel.</li>
-     * <li>{@code mac} &mdash; For any Apple Mac version.</li>
-     * <li>{@code freebsd} &mdash; For any FreeBSD version.</li>
-     * <li>{@code other} &mdash; For any other non-identified Operational System.</li>
-     * </ul>
-     *
-     * @return The identifier for the current Operational System.
-     */
-    private static @NotNull String getOsIdentifier() {
-        if (SystemUtils.IS_OS_WINDOWS) return "windows";
-        if (SystemUtils.IS_OS_LINUX) return "linux";
-        if (SystemUtils.IS_OS_MAC) return "mac";
-        if (SystemUtils.IS_OS_FREE_BSD) return "freebsd";
-
-        return "other";
-    }
-
-    /**
-     * Gets the identifier for the current processor architecture. The exact identifiers are:
-     * <ul>
-     * <li>{@code x86} &mdash; For any x86-compatible processor.</li>
-     * <li>{@code ia64} &mdash; For Intel Itanium IA-64 processors.</li>
-     * <li>{@code ppc} &mdash; For PowerPC processors.</li>
-     * <li>{@code other} &mdash; For any other non-identified processor architecture.</li>
-     * </ul>
-     *
-     * @return The identifier for the current processor architecture.
-     */
-    private static @NotNull String getArchIdentifier() {
-        switch (ArchUtils.getProcessor().getType()) {
-            case X86:
-                return "x86";
-            case IA_64:
-                return "ia64";
-            case PPC:
-                return "ppc";
-            default:
-                return "other";
-        }
-    }
-
-    /**
-     * Gets the identifier for the current processor bitness. The exact identifiers are:
-     * <ul>
-     * <li>{@code bit32} &mdash; For 32-bit processors.</li>
-     * <li>{@code bit64} &mdash; For 64-bit processors.</li>
-     * <li>{@code other} &mdash; For any other non-identified processor bitness.</li>
-     * </ul>
-     *
-     * @return The identifier for the current processor bitness.
-     */
-    private static @NotNull String getBitnessIdentifier() {
-        switch (ArchUtils.getProcessor().getArch()) {
-            case BIT_32:
-                return "bit32";
-            case BIT_64:
-                return "bit64";
-            default:
-                return "other";
-        }
-    }
 
 }
---
+++ java/src/main/java/com/davipb/jamspell/PlatformUtils.java
@@ -0,0 +1,82 @@
+package com.davipb.jamspell;
+
+import lombok.val;
+import org.apache.commons.lang3.ArchUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.arch.Processor;
+import org.jetbrains.annotations.NotNull;
+
+/** Internal class with strictly static helper methods for platform-dependant behavior. */
+class PlatformUtils {
+    private PlatformUtils() { throw new AssertionError(); }
+
+    /**
+     * A '/'-delimited unique path for the current platform, suitable for loading platform-specific resources
+     * from the classpath. The generated path does not start nor end with a '/'.
+     * <p>
+     * The path consists of {@code <os>/<arch>/<bit>}, where {@code <os>} is an identifier for the current
+     * Operating System (Windows, Linux, etc), {@code <arch>} is an identifier for the current processor architecture
+     * (x86, Itanium, PowerPC, etc), and {@code <bit>} is an identifier for the current processor bitness
+     * (32-bit, 64-bit, etc).
+     *
+     * @see #OS_IDENTIFIER
+     * @see #ARCH_IDENTIFIER
+     * @see #BITNESS_IDENTIFIER
+     */
+    final static String PLATFORM_PATH;
+
+    /**
+     * The identifier for the current Operational System. The exact identifiers are:
+     * <ul>
+     * <li>{@code windows} &mdash; For any Microsoft Windows version.</li>
+     * <li>{@code linux} &mdash; For any system using the Linux kernel.</li>
+     * <li>{@code mac} &mdash; For any Apple Mac version.</li>
+     * <li>{@code freebsd} &mdash; For any FreeBSD version.</li>
+     * <li>{@code other} &mdash; For any other non-identified Operational System.</li>
+     * </ul>
+     */
+    final static @NotNull String OS_IDENTIFIER;
+
+    /**
+     * The identifier for the current processor architecture. The exact identifiers are:
+     * <ul>
+     * <li>{@code x86} &mdash; For any x86-compatible processor.</li>
+     * <li>{@code ia64} &mdash; For Intel Itanium IA-64 processors.</li>
+     * <li>{@code ppc} &mdash; For PowerPC processors.</li>
+     * <li>{@code other} &mdash; For any other non-identified processor architecture.</li>
+     * </ul>
+     */
+    final static @NotNull String ARCH_IDENTIFIER;
+
+    /**
+     * The identifier for the current processor bitness. The exact identifiers are:
+     * <ul>
+     * <li>{@code bit32} &mdash; For 32-bit processors.</li>
+     * <li>{@code bit64} &mdash; For 64-bit processors.</li>
+     * <li>{@code other} &mdash; For any other non-identified processor bitness.</li>
+     * </ul>
+     */
+    final static @NotNull String BITNESS_IDENTIFIER;
+
+    static {
+
+        if (SystemUtils.IS_OS_WINDOWS) OS_IDENTIFIER = "windows";
+        else if (SystemUtils.IS_OS_LINUX) OS_IDENTIFIER = "linux";
+        else if (SystemUtils.IS_OS_MAC) OS_IDENTIFIER = "mac";
+        else if (SystemUtils.IS_OS_FREE_BSD) OS_IDENTIFIER = "freebsd";
+        else OS_IDENTIFIER = "other";
+
+        val arch = ArchUtils.getProcessor().getType();
+        if (arch == Processor.Type.X86) ARCH_IDENTIFIER = "x86";
+        else if (arch == Processor.Type.PPC) ARCH_IDENTIFIER = "ppc";
+        else if (arch == Processor.Type.IA_64) ARCH_IDENTIFIER = "ia64";
+        else ARCH_IDENTIFIER = "other";
+
+        val bitness = ArchUtils.getProcessor().getArch();
+        if (bitness == Processor.Arch.BIT_32) BITNESS_IDENTIFIER = "bit32";
+        else if (bitness == Processor.Arch.BIT_64) BITNESS_IDENTIFIER = "bit64";
+        else BITNESS_IDENTIFIER = "other";
+
+        PLATFORM_PATH = String.join("/", OS_IDENTIFIER, ARCH_IDENTIFIER, BITNESS_IDENTIFIER);
+    }
+}
